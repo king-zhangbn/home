1c5af1548a117fad6a8ae179a746a1344f5bb49a
king-zhangbn <zbn@bnnk.cc>
2023-02-06 10:28:25 +0800

    11

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -46,32 +46,32 @@ let linksData = [
   {
     icon: Blog,
     name: "博客",
-    link: "https://blog.imsyy.top/",
+    link: "https://en.icu/",
   },
   {
     icon: Cloud,
     name: "网盘",
-    link: "https://pan.imsyy.top/",
+    link: "https://pan.en.icu/",
   },
   {
     icon: CompactDisc,
     name: "音乐",
-    link: "https://music.imsyy.top/",
+    link: "https://yesplaymusic.en.icu/",
   },
   {
     icon: Compass,
     name: "起始页",
-    link: "https://nav.imsyy.top/",
+    link: "https://nav.en.icu",
   },
   {
     icon: Book,
     name: "网址集",
-    link: "https://web.imsyy.top/",
+    link: "https://website.en.icu/",
   },
   {
     icon: Flask,
-    name: "实验室",
-    link: "https://lab.imsyy.top/",
+    name: "服务",
+    link: "https://monitor.api.en.icu/",
   },
 ];
 
